dec317ffd8d3ca53967af7938274cbdabdd17167
davehypercodes <jesugbohunmi@outlook.com>
2023-08-11 05:28:32 +0000

    Add description to function main() in 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: 'prints to standard output using the puts() function'
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
