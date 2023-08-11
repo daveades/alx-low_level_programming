971091473365762a49583eee9ec40b7f0bac921f
davehypercodes <jesugbohunmi@outlook.com>
2023-08-11 05:31:15 +0000

    Add desription to function main() in 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: 'prints to standard output using the printf() function"
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
