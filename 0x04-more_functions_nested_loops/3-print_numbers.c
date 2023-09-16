8bed22bead9def904791375e1b92c4813eb51dcd
David Adesanya <jesugbohunmi@outlook.com>
2023-09-16 19:13:57 +0100

    Add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9 to stdout
+ */
+
+void print_numbers(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		_putchar(digit);
+	}
+	_putchar('\n');
+}
