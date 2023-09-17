3d6a214416ab16cb33d48d0f2da48f0ad820a2d8
David Adesanya <jesugbohunmi@outlook.com>
2023-09-17 17:41:07 +0100

    Add 7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+	if (n < 1)
+	{
+		_putchar(\n);
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			if (i > 1)
+			{
+				for (j = 1; j < i; j++)
+				{
+					_putchar(' ');
+				}	
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
