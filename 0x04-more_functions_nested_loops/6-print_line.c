6953ee0a6aa60eaf60abf5c3883059fde323d782
David Adesanya <jesugbohunmi@outlook.com>
2023-09-17 15:39:41 +0100

    Add 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a straight line to stdout
+ * @n: The number of '_' that specifiesline length
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; n > 0 && i < n; i++)
+	{
+		_putchar('_');
+	}
+	putchar('\n');
+}
