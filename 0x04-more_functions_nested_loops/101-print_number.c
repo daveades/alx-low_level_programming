c5f96787ecc9520a5639a6d68c31b40e1052990b
David Adesanya <jesugbohunmi@outlook.com>
2023-09-18 13:57:03 +0100

    Add 101-print_numbers.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,47 @@
 #include "main.h"
 
 /**
- * print_number - print a number as char to stdout
+ * print_number - prints an integer as char
  *
- * @n: the number to be printed
+ * @n: the num to be printed
  */
 void print_number(int n)
 {
-	int placeval, digit;
+	int reverse, reverse2, digit, bits, max_num;
 
+	reverse = reverse2 = 0;
+	max_num = 1;
+	for (bits = (sizeof(int) * 8) - 1; bits > 0; bits--)
+		max_num = max_num * 2;
+	max_num -= 1;
 	if (n < 0)
 	{
 		n = n * -1;
 		_putchar('-');
 	}
-
-	placeval = 1;
-	while (n >= placeval * 10)
+	else if (n == 0)
+		_putchar('0');
+	while (n > 0)
 	{
-		placeval = placeval * 10;
+		digit = n % 10;
+		if (reverse < (max_num / 100))
+			reverse = reverse * 10 + digit;
+		else
+			reverse2 = reverse2 * 10 + digit;
+		n /= 10;
 	}
-	while (placeval >= 1)
+	while (reverse > 0 || reverse2 > 0)
 	{
-		digit = (n / placeval) % 10;
+		if (reverse2 > 0)
+		{
+			digit = reverse2 % 10;
+			reverse2 /= 10;
+		}
+		else if (reverse > 0)
+		{
+			digit  = reverse % 10;
+			reverse /= 10;
+		}
 		_putchar(digit + '0');
-		placeval  = placeval / 10;
 	}
 }
