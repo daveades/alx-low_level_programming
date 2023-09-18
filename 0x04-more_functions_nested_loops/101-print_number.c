c4391276cffba6fd627445038dcec9f126746183
David Adesanya <jesugbohunmi@outlook.com>
2023-09-18 14:10:37 +0100

    Add 101-print_numbers.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,47 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer as char
- *
- * @n: the num to be printed
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
-	int reverse, reverse2, digit, bits, max_num;
+	unsigned int n1;
 
-	reverse = reverse2 = 0;
-	max_num = 1;
-	for (bits = (sizeof(int) * 8) - 1; bits > 0; bits--)
-		max_num = max_num * 2;
-	max_num -= 1;
 	if (n < 0)
 	{
-		n = n * -1;
+		n1 = -n;
 		_putchar('-');
-	}
-	else if (n == 0)
-		_putchar('0');
-	while (n > 0)
+	} else
 	{
-		digit = n % 10;
-		if (reverse < (max_num / 100))
-			reverse = reverse * 10 + digit;
-		else
-			reverse2 = reverse2 * 10 + digit;
-		n /= 10;
+		n1 = n;
 	}
-	while (reverse > 0 || reverse2 > 0)
+
+	if (n1 / 10)
 	{
-		if (reverse2 > 0)
-		{
-			digit = reverse2 % 10;
-			reverse2 /= 10;
-		}
-		else if (reverse > 0)
-		{
-			digit  = reverse % 10;
-			reverse /= 10;
-		}
-		_putchar(digit + '0');
+		print_number(n1 / 10);
 	}
+
+	_putchar((n1 % 10) + '0');
 }
