e060e0e1b7abcdc69e0d7d5fff9eb91f68249276
David Adesanya <jesugbohunmi@outlook.com>
2023-09-28 23:35:38 +0100

    Add 0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,15 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s = s + 1;
-	if (*s != 0)
-	{
-		_puts_recursion(s);
-	}
-	else
+	if (*s == 0)
 	{
 		_putchar('\n');
 	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
-
