2ebb89f8932beddc2e09c07488192886df19783f
davehypercodes <jesugbohunmi@outlook.com>
2023-10-06 15:45:29 +0000

    add 0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,14 @@
 
 /**
  * main - prints the name of the program to stdout
- * @argc - argument count
- * @argc - array of pointers to string (pointer to pointer to char)
+ * @argc: argument count
+ * @argv: array of pointers to string (pointer to pointer to char)
  *
  * Return: Always 0.
  */
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
