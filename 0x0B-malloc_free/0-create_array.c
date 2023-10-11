d6981c9893d1dc1065afa0ecb90ceb418b1caaa2
David Adesanya <jesugbohunmi@outlook.com>
2023-10-11 14:44:21 +0100

    Add 0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,22 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	int *arr;
+	char *arr;
+	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
+	arr = (char *)malloc(sizeof(char) * 98);
 	if  (arr == NULL)
 	{
 		return (NULL);
 	}
-	arr = (char *)malloc(sizeof(char) * 98);
-	arr[0] = c;
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
 	return (arr);
 }
 
