7ed80b466c53c30b06ab1b0f373923b4c8d7ccbb
Adesanya David <jesugbohunmi@outlook.com>
2023-11-17 21:13:07 +0100

    Add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: cmd argument count
+ * @argv: Array of strings - cmd arguments
+ * Return: Always 0.
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
+
