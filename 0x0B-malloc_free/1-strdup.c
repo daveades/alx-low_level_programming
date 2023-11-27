ebc02e911c6b1187da28f3df8395e2e11b834ef3
Adesanya David <jesugbohunmi@outlook.com>
2023-11-27 12:49:43 +0100

    Fix implicit declaration of _strlen

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,7 +12,7 @@ char *_strdup(char *str)
 	char *new_str;
 	int size;
 
-	if (*str == NULL)
+	if (str == NULL)
 	{
 		return (NULL);
 	}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,4 +4,5 @@
 char *create_array(unsigned int size, char c);
 char _putchar(char c);
 char *_strdup(char *str);
+int _strlen(char *str);
 #endif
