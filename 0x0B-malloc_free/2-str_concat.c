cbc53ff1978e972f434d063f4f0db7b48ee7b906
Adesanya David <jesugbohunmi@outlook.com>
2023-12-01 12:33:45 +0100

    Correct failing tests

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * str_concat - concatenates two strings
+ * @s1: first String
+ * @s2: second string
+ *
+ * Return: NULL if fail else char*
+ */
 char *str_concat(char *s1, char *s2)
 {
 	int total_size, i, size_1, size_2;
@@ -23,11 +30,11 @@ char *str_concat(char *s1, char *s2)
 	second_string_index = 0;
 	for (i = size_1; i < total_size - 1; i++)
 	{
-		str_concat[size_1] = s2[second_string_index];
+		str_concat[i] = s2[second_string_index];
 		second_string_index++;
 	}
 	str_concat[total_size - 1] = '\0';
-	return(str_concat);
+	return (str_concat);
 }
 
 /**
