221d2ebfe7c2393b14f100af65d196a8b459d60b
Adesanya David <jesugbohunmi@outlook.com>
2023-12-01 12:27:02 +0100

    Add 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 	second_string_index = 0;
 	for (i = size_1; i < total_size - 1; i++)
 	{
-		str_concat[size_1] = s2[second_string_index];
+		str_concat[i] = s2[second_string_index];
 		second_string_index++;
 	}
 	str_concat[total_size - 1] = '\0';
