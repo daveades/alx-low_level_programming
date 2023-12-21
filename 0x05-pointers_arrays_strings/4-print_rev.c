09de0c60044b1ea1c7532816e8e7515c21430136
Adesanya David <jesugbohunmi@outlook.com>
2023-12-22 00:31:45 +0100

    Update print_rev function to use custom header file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
 * print_rev - Prints a string in reverse.
@@ -6,23 +6,25 @@
 */
 void print_rev(char *s)
 {
+	int length;
+	int i; /* Declare the loop variable before the loop */
+
+	length = 0;
 	if (s == NULL)
 	{
-	return; /* handle NULL pointer */
+		return; /* handle NULL pointer */
 	}
 
-	int length = 0;
 
 	while (s[length] != '\0')
 	{
-	length++; /* calculate the length of the string */
+		length++; /* calculate the length of the string */
 	}
 
-	for (int i = length - 1; i >= 0; i--)
-
+	for (i = length - 1; i >= 0; i--)
 	{
-	putchar(s[i]); /* print characters in reverse order */
+		_putchar(s[i]); /* print characters in reverse order */
 	}
 
-	putchar('\n'); /* print a new line */
+	_putchar('\n'); /* print a new line */
 }
