fff63263dd33c9a16a38629034074f03f1ff8d82
Dave <jesugbohunmi@outlook.com>
2023-12-25 21:41:26 +0100

    Add _strpbrk function

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,4 +6,5 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
 #endif
