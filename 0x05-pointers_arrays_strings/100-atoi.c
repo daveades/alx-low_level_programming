a49009d07dc36894c17f1fadce5169889872809a
DAVE <jesugbohunmi@outlook.com>
2023-12-29 14:18:00 +0100

    Add check for overflow in _atoi function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,9 @@
+#include <limits.h>
+
 int _atoi(char *s)
 {
 	int sign = 1;
-
 	int result = 0;
-
 	int i = 0;
 
 	/* Check for negative sign */
@@ -19,6 +19,15 @@ int _atoi(char *s)
 		/* Check if character is a digit */
 		if (s[i] >= '0' && s[i] <= '9')
 		{
+			/* Check for overflow */
+			if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+			{
+				if (sign == 1)
+					return INT_MAX;
+				else
+					return INT_MIN;
+			}
+
 			/* Convert character to integer and add to result */
 			result = result * 10 + (s[i] - '0');
 		}
@@ -38,5 +47,5 @@ int _atoi(char *s)
 	/* Apply the sign to the result */
 	result *= sign;
 
-	return (result);
+	return result;
 }
