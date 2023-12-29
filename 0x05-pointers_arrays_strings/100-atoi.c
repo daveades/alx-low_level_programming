4c12c330938dac4dc6f166e2e1b426f926e2d345
DAVE <jesugbohunmi@outlook.com>
2023-12-29 14:50:56 +0100

    Refactor _atoi function to improve readability and remove unnecessary helper function

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,17 +1,8 @@
 #include "main.h"
-
-/**
-* _atoi - Converts a string to an integer
-* @s: The string to be converted
-*
-* Return: The integer value of the converted string
-*/
 int _atoi(char *s)
 {
 	int sign = 1;
-
 	int result = 0;
-
 	int i = 0;
 
 	/* Check for negative sign */
@@ -21,26 +12,6 @@ int _atoi(char *s)
 		i++;
 	}
 
-	result = convertStringToInt(s, sign, result, i);
-
-	/* Apply the sign to the result */
-	result *= sign;
-
-	return (result);
-}
-
-
-/**
- * convertStringToInt - Converts a string to an integer.
- * @s: The string to be converted.
- * @sign: The sign of the integer.
- * @result: The initial result value.
- * @i: The index of the string.
- *
- * Return: The integer value of the converted string.
- */
-int convertStringToInt(char *s, int sign, int result, int i)
-{
 	/* Iterate through the string */
 	for (; s[i] != '\0'; i++)
 	{
@@ -48,13 +19,12 @@ int convertStringToInt(char *s, int sign, int result, int i)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			/* Check for overflow */
-			if (result > INT_MAX / 10 ||
-			(result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+			if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
 			{
 				if (sign == 1)
-					return (INT_MAX);
+					return INT_MAX;
 				else
-					return (INT_MIN);
+					return INT_MIN;
 			}
 
 			/* Convert character to integer and add to result */
@@ -62,14 +32,18 @@ int convertStringToInt(char *s, int sign, int result, int i)
 		}
 		else
 		{
+			/* Stop parsing the string if a non-digit character is encountered after parsing a digit character */
 			if (result != 0)
 				break;
-
+			
 			/* Check for consecutive signs */
 			if (s[i] == '-')
 				sign *= -1;
 		}
 	}
 
-return (result);
+	/* Apply the sign to the result */
+	result *= sign;
+
+	return result;
 }
