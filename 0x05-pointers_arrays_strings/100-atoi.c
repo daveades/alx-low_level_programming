62b082b7a432b319b866b1673669aaced8da726c
DAVE <jesugbohunmi@outlook.com>
2023-12-29 11:18:15 +0100

    Add _atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i;
+
+	/* Check for negative sign */
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+
+	/* Iterate through the string */
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/* Check if character is a digit */
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			/* Convert character to integer and add to result */
+			result = result * 10 + (s[i] - '0');
+		}
+		else
+		{
+			/* Break if non-digit character is encountered */
+			break;
+		}
+	}
+
+	return (sign * result);
+}
