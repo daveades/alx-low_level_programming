94c8152c6c0f891d6251064bc408fa7bea11db4c
DAVE <jesugbohunmi@outlook.com>
2023-12-29 06:13:04 +0100

    Refactor square root calculation to handle negative numbers and improve efficiency

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,43 +1,43 @@
-#include "main.h"
-
 /**
-* _sqrt_recursion - Returns the natural square root of a number.
-* @n: The number to find the square root of.
-*
-* Return: The square root of n, or -1 if n does not have a natural square root.
-*/
+ * _sqrt_recursion - Calculates the square root of a number recursively.
+ * @n: The number to calculate the square root of.
+ *
+ * Return: The square root of n,
+ * or -1 if n does not have a natural square root.
+ */
 int _sqrt_recursion(int n)
 {
-	unsigned int k;
-
-	k = n;
-	if (k == 0 || k == 1)
-		return (k);
-	return (sqrt_helper(k, 1, k));
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrt_helper(n, 1, n));
 }
 
 /**
-* sqrt_helper - Recursive helper function to find the square root.
-* @n: The number to find the square root of.
+* sqrt_helper - Helper function for calculating the square root recursively.
+* @n: The number to calculate the square root of.
 * @start: The starting point for the search.
 * @end: The ending point for the search.
 *
 * Return: The square root of n,
 * or -1 if n does not have a natural square root.
 */
-
-unsigned int sqrt_helper(unsigned int n, int start, unsigned int end)
+long int sqrt_helper(long int n, long int start, long int end)
 {
-	int mid;
+	int mid = (start + end) / 2;
 
-	if (start <= end)
+	if (mid * mid == n)
+		return (mid);
+	if (mid * mid < n)
 	{
-		mid = (start + end) / 2;
-		if (mid * mid == n)
+		if ((mid + 1) * (mid + 1) > n)
 			return (mid);
-		if (mid * mid < n)
+		else
 			return (sqrt_helper(n, mid + 1, end));
+	}
+	else
+	{
 		return (sqrt_helper(n, start, mid - 1));
 	}
-	return (-1);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,6 +8,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int sqrt_helper(int n, int start, int end);
+long int sqrt_helper(int n, int start, int end);
 
 #endif
