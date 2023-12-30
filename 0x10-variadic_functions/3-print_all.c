28dc36672bd50a01116efce3627e82f7d6711cc1
DAVE <jesugbohunmi@outlook.com>
2023-12-30 01:21:56 +0100

    Refactor print_all function to handle additional format specifiers

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,46 +1,45 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - Prints anything.
- * @format: A string of format specifiers.
- * @...: The variadic arguments to be printed.
- * Return: void
+ * print_all - Prints values based on a given format.
+ * @format: The format string.
+ * ...
  */
-void print_all(const char *const format, ...)
+void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0;
 	char *s;
 	char c;
+	float f;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		switch (format[i])
 		{
-			c = va_arg(args, int);
-			printf("%c", c);
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%d", va_arg(args, int));
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%f", va_arg(args, double));
-		}
-		else if (format[i] == 's')
-		{
-			s = va_arg(args, char *);
-			if (s == NULL)
-				printf("(nil)");
-			else
-				printf("%s", s);
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(args, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					printf("(nil)");
+				if (s != NULL)
+					printf("%s", s);
+				break;
 		}
 		i++;
-		if (format[i])
-			printf(", ");
 	}
 
 	va_end(args);
