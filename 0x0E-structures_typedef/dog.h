700f16df8c71de67a48f01ade6d2352e48bcfc21
DAVE <jesugbohunmi@outlook.com>
2023-12-31 19:51:49 +0100

    Refactor code to improve performance and readability

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+#include <stdio.h>
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* DOG_H */
