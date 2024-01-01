d19053cc37941dd6801fb51af5711185bd8998fc
DAVE <jesugbohunmi@outlook.com>
2024-01-01 23:17:42 +0100

    Add ELF header printing function

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		print_error("Usage: elf_header elf_filename");
+	}
+
+	print_elf_header(argv[1]);
+
+	return 0;
+}
+
+void print_error(const char *message)
+{
+	fprintf(stderr, "%s\n", message);
+	exit(98);
+}
+
+void print_elf_header(const char *filename)
+{
+	int fd = open(filename, O_RDONLY);
+	Elf64_Ehdr header;
+
+	if (fd == -1)
+	{
+		print_error("Error opening file");
+	}
+
+	if (read(fd, &header, sizeof(header)) != sizeof(header))
+	{
+		print_error("Error reading ELF header");
+	}
+
+	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+	    header.e_ident[EI_MAG1] != ELFMAG1 ||
+	    header.e_ident[EI_MAG2] != ELFMAG2 ||
+	    header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		print_error("Not an ELF file");
+	}
+
+	printf("Magic: %c%c%c%c\n",
+	       header.e_ident[EI_MAG1],
+	       header.e_ident[EI_MAG2],
+	       header.e_ident[EI_MAG3],
+	       header.e_ident[EI_MAG0]);
+
+	printf("Class: %s\n", header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+
+	printf("Data: %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+
+	printf("Version: %d\n", header.e_ident[EI_VERSION]);
+
+	printf("OS/ABI: %d\n", header.e_ident[EI_OSABI]);
+
+	printf("ABI Version: %d\n", header.e_ident[EI_ABIVERSION]);
+
+	printf("Type: %d\n", header.e_type);
+
+	printf("Entry point address: 0x%lx\n", header.e_entry);
+
+	close(fd);
+}
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <elf.h>
 
 #define BUFFER_SIZE 1024
 
@@ -16,5 +17,7 @@ int append_text_to_file(const char *filename, char *text_content);
 void copy_file(int fd_from, int fd_to, char *buffer,
 char *file_from, char *file_to);
 void close_files(int fd_from, int fd_to);
+void print_error(const char *message);
+void print_elf_header(const char *filename);
 
 #endif /* MAIN_H */
