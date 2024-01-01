20d0e1908125758d82bb4b06ac6c7d337708f2a8
DAVE <jesugbohunmi@outlook.com>
2024-01-01 13:40:48 +0100

    Fix memory leak and improve list traversal in print_listint_safe() function

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,20 +10,29 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *current = head;
 	size_t count = 0;
+	const listint_t **addr_store = malloc(sizeof(listint_t *) * 1024);
+	size_t i;
+
+	if (addr_store == NULL)
+		return (0);
 
 	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-
-		if (current <= current->next)
+		for (i = 0; i < count; i++)
 		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			break;
+			if (current == addr_store[i])
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				free(addr_store);
+				return (count);
+			}
 		}
 
+		printf("[%p] %d\n", (void *)current, current->n);
+		addr_store[count++] = current;
 		current = current->next;
 	}
 
+	free(addr_store);
 	return (count);
 }
