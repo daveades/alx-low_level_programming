b72674472248d4571e666743e85f4a45da1b8c3b
DAVE <jesugbohunmi@outlook.com>
2024-01-01 23:21:39 +0100

    Add main function documentation and improve code formatting

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
 
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: 0 on success, non-zero on failure.
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
@@ -10,18 +17,27 @@ int main(int argc, char *argv[])
 
 	print_elf_header(argv[1]);
 
-	return 0;
+	return (0);
 }
 
+/**
+* print_error - Prints an error message and exits.
+* @message: The error message to print.
+*/
 void print_error(const char *message)
 {
 	fprintf(stderr, "%s\n", message);
 	exit(98);
 }
 
+/**
+* print_elf_header - Prints the ELF header of a given file.
+* @filename: The name of the file.
+*/
 void print_elf_header(const char *filename)
 {
 	int fd = open(filename, O_RDONLY);
+
 	Elf64_Ehdr header;
 
 	if (fd == -1)
@@ -48,20 +64,17 @@ void print_elf_header(const char *filename)
 	       header.e_ident[EI_MAG3],
 	       header.e_ident[EI_MAG0]);
 
-	printf("Class: %s\n", header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("Class: %s\n", header.e_ident[EI_CLASS] == ELFCLASS32 ?
+	"ELF32" : "ELF64");
 
-	printf("Data: %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+	printf("Data: %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ?
+	"2's complement, little endian" : "2's complement, big endian");
 
 	printf("Version: %d\n", header.e_ident[EI_VERSION]);
-
 	printf("OS/ABI: %d\n", header.e_ident[EI_OSABI]);
-
 	printf("ABI Version: %d\n", header.e_ident[EI_ABIVERSION]);
-
 	printf("Type: %d\n", header.e_type);
-
 	printf("Entry point address: 0x%lx\n", header.e_entry);
-
 	close(fd);
 }
 
