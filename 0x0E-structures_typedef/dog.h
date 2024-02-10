a28ab9e2b2617a1984d62395ac3bab183a657b4a
DAVE <jesugbohunmmi@outlook.com>
2024-02-10 13:18:48 +0100

    Add dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,27 +2,18 @@
 #define DOG_H
 
 /**
- * struct dog - Structure representing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: dog structure
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/* Function prototypes */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif /* DOG_H */
-
+#endif
