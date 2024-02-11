d126abfd07c7d467776b203d8337b820a42667c4
DAVE <jesugbohunmmi@outlook.com>
2024-02-11 12:01:45 +0100

    Add declarattion for func new_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,8 +20,10 @@ typedef struct dog
 /* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 /* Standard Library */
 #include <stdio.h>
+#include <stdlib.h>
 
 #endif
