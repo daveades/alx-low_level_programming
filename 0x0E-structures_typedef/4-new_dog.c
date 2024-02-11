e40ad6c834d0d01314cbf34075a3e61cde2e7f8f
DAVE <jesugbohunmmi@outlook.com>
2024-02-11 12:05:59 +0100

    Remove trailing whitespace on line 13

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,9 +1,16 @@
 #include "dog.h"
-
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	
+
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
