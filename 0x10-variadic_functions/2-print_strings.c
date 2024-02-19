503185670e0d4520f568024fbe612b13f399e8e7
DAVE <jesugbohunmmi@outlook.com>
2024-02-19 13:45:44 +0100

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", s);
 			if (separator != NULL && i < n - 1)
 			{
-				printf("%s ", separator);
+				printf("%s", separator);
 			}
 		}
 		else
