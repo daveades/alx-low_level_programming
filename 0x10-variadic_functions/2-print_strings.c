ff98d4cfdd6480361b028cabb0cc088e405eabad
DAVE <jesugbohunmmi@outlook.com>
2024-02-19 13:17:31 +0100

    Add statement to check if any input string is NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,16 +9,21 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	char *s;
 	va_list args;
 	unsigned int i;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(args, char *));
-		if (separator != NULL && i < n - 1)
+		s = va_arg(args, char *);
+		if (s != NULL)
 		{
-			printf("%s ", separator);
+			printf("%s", s);
+			if (separator != NULL && i < n - 1)
+			{
+				printf("%s ", separator);
+			}
 		}
 	}
 	printf("\n");
