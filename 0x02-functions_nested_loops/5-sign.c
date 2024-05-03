5c7f5e5ef48c8aab7ed59c0182a3d1b806601738
DAVE <jesugbohunmi@outlook.com>
2024-05-03 20:50:55 +0100

    Add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sing of a number
+ * @n: number whose sign is to be printed
+ *
+ * Return: 1 if postive, 0 if 0 and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
