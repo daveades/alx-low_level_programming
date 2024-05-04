91bb324184ff5fb85f8537c69e5da9f8f7bfa208
DAVE <jesugbohunmi@outlook.com>
2024-05-04 14:59:29 +0100

    Add 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,26 +1,54 @@
-#include "main.h"
-#define PASSWORD_LENGTH 10
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Generates valid password for
+ * the 101-crackme program
+ * Return: Always 0.
  */
+
 int main(void)
 {
-	char password[PASSWORD_LENGTH + 1];
-	int i;
+	char password[84];
+	int index = 0, sum = 0, half1, half2;
 
-	srand(time(NULL));
+	srand(time(0));
 
-	for (i = 0; i < PASSWORD_LENGTH; i++)
+	while (sum < 2772)
 	{
-		password[i] = rand() % 94 + 33;
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
 	}
+	password[index] = '\0';
 
-	password[PASSWORD_LENGTH] = '\0';
+	if (sum != 2772)
+	{
+		half1 = (sum - 2772) / 2;
+		half2 = (sum - 2772) / 2;
 
-	printf("%s\n", password);
+		if ((sum - 2772) % 2 != 0)
+		{
+			half1++
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + half1))
+			{
+				password[index] -= half1;
+				break;
+			}
+		}
 
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + half2))
+			{
+				password[index] -= half2;
+				break;
+			}
+		}
+	}
+	printf("%s", password);
 	return (0);
 }
