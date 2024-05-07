8c3fdc81ff44582cbce029ff7f01025c6d3ae918
DAVE <jesugbohunmi@outlook.com>
2024-05-07 11:23:36 +0100

    Add 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Update the value of a pointer to an int
+ * n: pointer to an int
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
