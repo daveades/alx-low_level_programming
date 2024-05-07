58a2a7dee8fc5550a34464419a528f8c6fbf097e
DAVE <jesugbohunmi@outlook.com>
2024-05-07 17:06:36 +0100

    Add 5-print_rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: string to be reversed
+ *
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int strlen, i, j, a;
+	char str[];
+
+	strlen = _strlen(s);
+	for (a = 0; a == strlen; a++)
+	{
+		str[a] = s[a];
+	}
+
+	for (i = strlen - 1, j = 0; i < 0; i--, j++)
+	{
+		s[j] = str[i];
+	}
+}
+
+/**
+ * _strlen - Determines the length of a string
+ * @s: pointer to string
+ *
+ * Return: number of bytes occupied by the string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
