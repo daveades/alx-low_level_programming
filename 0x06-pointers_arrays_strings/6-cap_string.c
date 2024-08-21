780922d25c3dc6150a22b44dac29d57afbea5ee0
David Adesanya <jesugbohunmi23@gmail.com>
2024-08-21 15:46:39 +0000

    Correct C++ style comments, betty errors

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,25 +3,32 @@
 /**
  * cap_string - Capitalizes all words of a string
  * @str: The string to capitalize
- * 
+ *
  * Return: The capitalized string
  */
-char *cap_string(char *str) {
-    int capitalize_next = 1; // Flag to indicate if the next character should be capitalized
+char *cap_string(char *str)
+{
+	int capitalize_next = 1;
 
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (capitalize_next && str[i] >= 'a' && str[i] <= 'z') {
-            str[i] = str[i] - ('a' - 'A'); // Convert to uppercase
-            capitalize_next = 0;
-        } else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||
-                   str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
-                   str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' ||
-                   str[i] == '}') {
-            capitalize_next = 1;
-        } else {
-            capitalize_next = 0;
-        }
-    }
-
-    return str;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+			capitalize_next = 0;
+		}
+		else if (str[i] == ' ' || str[i] == '\t'
+				|| str[i] == '\n' || str[i] == ',' || str[i] == ';'
+				|| str[i] == '.' || str[i] == '!' || str[i] == '?'
+				|| str[i] == '"' || str[i] == '(' || str[i] == ')'
+				|| str[i] == '{' || str[i] == '}')
+		{
+			capitalize_next = 1;
+		}
+		else
+		{
+			capitalize_next = 0;
+		}
+	}
+	return (str);
 }
